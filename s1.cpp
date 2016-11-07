c58ea4ac19d38d9bf2fc0e726adf7d0b977f0180
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-11-07 00:33:21 -0500

    Calculate centroid and radius and output it

--- s1.cpp
+++ s1.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
@@ -22,6 +23,21 @@ int main(int argc, char *argv[]) {
   Mat binaryImage;
   applyBinary(image, binaryImage, threshold);
 
+  vector<Point> TRBLArray = getTRBLArrayOfSphere(binaryImage);
+  Point top = TRBLArray[0];
+  Point right = TRBLArray[1];
+  Point bot = TRBLArray[2];
+  Point left = TRBLArray[3];
+
+  // Center row == avg(right_row, left_row)
+  int center_row = (right.x + left.x) / 2;
+  // Center col == avg(top_col, bot_col)
+  int center_col = (top.y + bot.y) / 2;
+  // radius == avg(diameters) / 2
+  int verticalDiameter = sqrt(pow(top.x - bot.x, 2) + pow(top.y - bot.y, 2));
+  int horizontalDiameter = sqrt(pow(left.x - right.x, 2) + pow(left.y - right.y, 2));
+  int radius = (verticalDiameter + horizontalDiameter) / 2 / 2;
+
   namedWindow("Original Image", WINDOW_AUTOSIZE);
   namedWindow("Binary Image", WINDOW_AUTOSIZE);
 
@@ -30,7 +46,7 @@ int main(int argc, char *argv[]) {
 
   waitKey(0);
 
-  parametersFile << 11 << " " << 17 << endl;
+  parametersFile << center_row << " " << center_col << " " << radius << endl;
   parametersFile.close();
 
   return 0;
