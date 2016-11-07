5400ea65a6a630221421fcf20dea21a0c865d231
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-11-06 23:29:14 -0500

    Implement method to locate edges of sphere

--- Utilities.hpp
+++ Utilities.hpp
@@ -23,4 +23,25 @@ bool imageValidityCheck(Mat &image) {
   return true;
 }
 
+vector<Point> getTRBLArrayOfSphere(Mat &image) {
+  Point top = Point(image.rows - 1, image.cols - 1);
+  Point right = Point(image.rows - 1, 0);
+  Point bottom = Point(0, 0);
+  Point left = Point(0, image.cols - 1);
+
+  for (int r = 0; r < image.rows; r++) {
+    for (int c = 0; c < image.cols; c++) {
+      if (image.at<uchar>(r, c)) {
+        if (r < top.x) top = Point(r, c);       // if higher than top
+        if (c > right.y) right = Point(r, c);   // if more right
+        if (r > bottom.x) bottom = Point(r, c); // if lower than bottom
+        if (c < left.y) left = Point(r, c);     // if more left
+      }
+    }
+  }
+
+  vector<Point> TRBL = {top, right, bottom, left};
+  return TRBL;
+}
+
 #endif
