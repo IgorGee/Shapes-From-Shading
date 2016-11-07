5b9f91583cdb07ba55c101122ea377a262302b5b
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-11-07 12:52:24 -0500

    Implement s3 basis

---
+++ s3.cpp
@@ -0,0 +1,31 @@
+#include "Utilities.hpp"
+#include "Filters.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+  if (!argsS3Check(argc)) return -1;
+
+  ifstream directionsFile(argv[1]);
+  vector<string> imageFiles = {argv[2], argv[3], argv[4]};
+  int step = atoi(argv[5]);
+  int threshold = atoi(argv[6]);
+  ofstream needleOutputFile(argv[7]);
+
+  vector<Mat> images;
+  for (auto imageName : imageFiles) {
+    Mat image = imread(imageName, CV_LOAD_IMAGE_GRAYSCALE);
+    if (!imageValidityCheck(image)) return -1;
+    CV_Assert(image.depth() == CV_8U);
+    images.push_back(image);
+  }
+
+  return 0;
+}
